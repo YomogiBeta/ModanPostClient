9316fb1a275ebe1746dd12a586399f3819b0bdee
YomogiBeta <yomogibeta5829@gmail.com>
2023-01-25 07:10:41 +0900

    add post input maxlength validation

--- components/PostInputField/resolver.ts
+++ components/PostInputField/resolver.ts
@@ -2,8 +2,8 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup';
 
 const schema = yup.object({
-  title: yup.string().min(2,"2文字以上入力する必要があります").required("必須の項目です"),
-  content: yup.string().min(2,"2文字以上入力する必要があります").required("必須の項目です")
+  title: yup.string().min(2,"2文字以上入力する必要があります").max(30, "タイトルは最大30文字です").required("必須の項目です"),
+  content: yup.string().min(2,"2文字以上入力する必要があります").max(1000, "内容は最大1000文字です").required("必須の項目です")
 })
 
 export default yupResolver(schema)
