72be6cc60c58add2bb477b6e611f00a5dd20ae0c
YomogiBeta <yomogibeta5829@gmail.com>
2023-01-26 06:00:42 +0900

    change post content max length

--- components/PostInputField/resolver.ts
+++ components/PostInputField/resolver.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 
 const schema = yup.object({
   title: yup.string().min(2,"2文字以上入力する必要があります").max(30, "タイトルは最大30文字です").required("必須の項目です"),
-  content: yup.string().min(2,"2文字以上入力する必要があります").max(1000, "内容は最大1000文字です").required("必須の項目です")
+  content: yup.string().min(2,"2文字以上入力する必要があります").max(2400, "内容は最大2400文字です").required("必須の項目です")
 })
 
 export default yupResolver(schema)
